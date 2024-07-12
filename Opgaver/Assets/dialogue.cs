60ea094583cb538a832323a5d4762c5db2edc5c0
Deltager <teknisk+GDC2024-GR6@game.unf.dk>
2024-07-12 14:51:06 +0200

    test1scenegonewrong
    blahblah

---
+++ Opgaver/Assets/dialogue.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hej, jeg er ond");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
